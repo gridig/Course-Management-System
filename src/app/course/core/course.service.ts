916fcab23f9e32770071dbb654ea35b1e43e5eca
gridig <igorabdul@gmail.com>
2019-10-23 20:12:01 +0200

    dead app

--- src/app/course/core/course.service.ts
+++ src/app/course/core/course.service.ts
@@ -3,27 +3,33 @@ import { HttpClient } from '@angular/common/http';
 
 import { CourseModel } from './course.model';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 
 export class CourseService {
+
+  url: string;
+
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+    this.url = `${environment.API_URL}courses`;
+   }
 
   getCourses(): Observable<any> {
-    return this.http.get('http://localhost:3000/courses');
+    return this.http.get('');
   }
 
   createCourse(course: CourseModel) {
-    return this.http.post('http://localhost:3000/courses', course);
+    return this.http.post(this.url, course);
   }
 
   editCourse(id: number, course: CourseModel) {
-    return this.http.put('http://localhost:3000/courses/' + id, course);
+    return this.http.put(`${this.url}/${id}`, course);
   }
 
   deleteCourse(id: number) {
-    return this.http.delete('http://localhost:3000/courses/' + id);
+    return this.http.delete(`${this.url}/${id}`);
   }
 }
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -7,6 +7,7 @@ import { CourseService } from 'src/app/course/core/course.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   courses: CourseModel[];
